b1660040eb753845dc13e6d2ee7106927dee51f3
vickysingh500001 <singh.vicky5001@gmail.com>
2018-07-18 14:25:26 +0530

    feature one changedd
    feature one changedd

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var url = require('url');
 http.createServer((req,res) => {
     console.log('server creayed');
     var q = url.parse(req.url,true).queryl;
+    if(req.url === '/submit') {
+        console.log('value fetched');
+    }
 }).listen(2000);
 
 
